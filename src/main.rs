0b3e9a50291523dff891f564680c0a38caf2a4ed
wdhg <wdhgrant@gmail.com>
2021-03-09 16:31:54 +0000

    add basic reading and writing to registers, and add ldr_r_r function

--- src/main.rs
+++ src/main.rs
@@ -30,28 +30,42 @@ struct GameBoy {
 
 impl GameBoy {
     fn new() -> GameBoy {
-        let mut registers8 = HashMap::new();
-        registers8.insert(Register8::A, 0);
-        registers8.insert(Register8::B, 0);
-        registers8.insert(Register8::C, 0);
-        registers8.insert(Register8::D, 0);
-        registers8.insert(Register8::E, 0);
-        registers8.insert(Register8::F, 0);
-        registers8.insert(Register8::H, 0);
-        registers8.insert(Register8::L, 0);
-
-        let mut registers16 = HashMap::new();
-        registers16.insert(Register16::PC, INITIAL_PC);
-        registers16.insert(Register16::SP, INITIAL_SP);
-
-        return GameBoy {
-            registers8: registers8,
-            registers16: registers16,
+        let mut gb = GameBoy {
+            registers8: HashMap::new(),
+            registers16: HashMap::new(),
             memory: [0; MEMORY_SIZE],
         };
+
+        gb.registers8.insert(Register8::A, 0);
+        gb.registers8.insert(Register8::B, 0);
+        gb.registers8.insert(Register8::C, 0);
+        gb.registers8.insert(Register8::D, 0);
+        gb.registers8.insert(Register8::E, 0);
+        gb.registers8.insert(Register8::F, 0);
+        gb.registers8.insert(Register8::H, 0);
+        gb.registers8.insert(Register8::L, 0);
+
+        gb.registers16.insert(Register16::PC, INITIAL_PC);
+        gb.registers16.insert(Register16::SP, INITIAL_SP);
+
+        return gb;
     }
 }
 
+fn read_register8(gb: &GameBoy, r: Register8) -> u8 {
+    return gb.registers8.get(&r).copied().unwrap_or_default();
+}
+
+fn write_register8(gb: &mut GameBoy, r: Register8, v: u8) {
+    gb.registers8.entry(r).and_modify(|e| *e = v).or_default();
+}
+
+// load register into register
+fn ld_r_r(gb: &mut GameBoy, to: Register8, from: Register8) {
+    let v = read_register8(gb, from);
+    write_register8(gb, to, v);
+}
+
 fn main() {
-    let gb = GameBoy::new();
+    let gb = &mut GameBoy::new();
 }
