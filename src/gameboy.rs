6399bc2bfb051b1ccd177fc687d036a03e209a03
wdhg <wdhgrant@gmail.com>
2021-03-25 16:08:53 +0000

    add read and write for 16 bit registers and add tests for both 8 and 16 bit

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -58,6 +58,17 @@ impl GameBoy {
     pub fn write_register8(&mut self, r: Reg8, v: u8) {
         self.registers8.entry(r).and_modify(|e| *e = v).or_default();
     }
+
+    pub fn read_register16(&self, r: Reg16) -> u16 {
+        return self.registers16.get(&r).copied().unwrap_or_default();
+    }
+
+    pub fn write_register16(&mut self, r: Reg16, v: u16) {
+        self.registers16
+            .entry(r)
+            .and_modify(|e| *e = v)
+            .or_default();
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -73,6 +84,39 @@ pub fn decode(opcode: u8) -> Instr {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_read_and_write_8_bit_registers() {
+        use Reg8::*;
+
+        let gb = &mut GameBoy::new();
+
+        gb.write_register8(A, 0x01);
+        gb.write_register8(F, 0x23);
+        gb.write_register8(L, 0x45);
+
+        assert_eq!(gb.read_register8(A), 0x01);
+        assert_eq!(gb.read_register8(B), 0x00);
+        assert_eq!(gb.read_register8(C), 0x00);
+        assert_eq!(gb.read_register8(D), 0x00);
+        assert_eq!(gb.read_register8(E), 0x00);
+        assert_eq!(gb.read_register8(F), 0x23);
+        assert_eq!(gb.read_register8(H), 0x00);
+        assert_eq!(gb.read_register8(L), 0x45);
+    }
+
+    #[test]
+    fn test_read_and_write_16_bit_registers() {
+        use Reg16::*;
+
+        let gb = &mut GameBoy::new();
+
+        gb.write_register16(PC, 0xabcd);
+        gb.write_register16(SP, 0xef01);
+
+        assert_eq!(gb.read_register16(PC), 0xabcd);
+        assert_eq!(gb.read_register16(SP), 0xef01);
+    }
 
     #[test]
     fn test_decode() {
