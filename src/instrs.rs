07228a886e43b0906cdcb6d1cb87d0d873aa2cda
wdhg <wdhgrant@gmail.com>
2021-03-25 16:44:22 +0000

    add all misc instructions

--- src/instrs.rs
+++ src/instrs.rs
@@ -1,17 +1,29 @@
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Instr {
-    NOP,
-    DAA,
-    HALT,
+    NOP,  // no operation
+    DAA,  // decimal adjust register A
+    CPL,  // complement register A (flip all bits)
+    CCF,  // complement carry flag
+    SCF,  // set carry flag
+    HALT, // power down CPU until an interrupt occurs
+    STOP, // halt CPU and LCD display until button pressed
+    DI,   // disables interrupts
+    EI,   // enables interrupts
 }
 
 use Instr::*;
 
-pub fn decode(opcode: u8) -> Instr {
+pub fn decode(opcode: (u8, u8)) -> Instr {
     match opcode {
-        0x00 => NOP,
-        0x27 => DAA,
-        0x76 => HALT,
+        (0x00, _) => NOP,
+        (0x27, _) => DAA,
+        (0x2F, _) => CPL,
+        (0x3F, _) => CCF,
+        (0x37, _) => SCF,
+        (0x76, _) => HALT,
+        (0x10, 0x00) => STOP,
+        (0xF3, _) => DI,
+        (0xFB, _) => EI,
         _ => NOP,
     }
 }
@@ -22,8 +34,9 @@ mod tests {
 
     #[test]
     fn test_decode() {
-        assert_eq!(decode(0x00), NOP);
-        assert_eq!(decode(0x27), DAA);
-        assert_eq!(decode(0x76), HALT);
+        assert_eq!(decode((0x00, 0x00)), NOP);
+        assert_eq!(decode((0x27, 0x00)), DAA);
+        assert_eq!(decode((0x76, 0x00)), HALT);
+        assert_eq!(decode((0x10, 0x00)), STOP);
     }
 }
